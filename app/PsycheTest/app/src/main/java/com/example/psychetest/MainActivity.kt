cdc56434638627373aebcbdb784d0a7ed519cda5
Dikshant <dikshant.073@gmail.com>
2023-10-29 14:03:08 +0530

    added final app

--- app/PsycheTest/app/src/main/java/com/example/psychetest/MainActivity.kt
+++ app/PsycheTest/app/src/main/java/com/example/psychetest/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.psychetest.databinding.ActivityMainBinding
 import com.example.psychetest.databinding.ActivityRegistrationBinding
+import com.google.android.gms.common.data.DataBufferSafeParcelable.addValue
 
 class MainActivity : AppCompatActivity() {
-
     private var binding: ActivityMainBinding? = null
+//    var mycalc = calc()
+//    public var dta : calc2 = calc2(0, 0, 0, 0, 0);
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,18 +20,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding?.root)
 
         binding?.OptA1?.setOnClickListener {
+//            addValues(mycalc, 0,20,0,0,0 )
+//            dta.addValues(0, 20, 0, 0, 0);
+            CALC2.addValues(0, 20, 0, 0, 0);
             navigateToQuestionTwo()
         }
 
         binding?.OptB1?.setOnClickListener {
+//            subtractValue(mycalc, 0,20,0,0,0 )
+//            dta.subtractValue(0, 20, 0, 0, 0);
+            CALC2.subtractValue(0, 20, 0, 0, 0);
             navigateToQuestionTwo()
+
         }
 
         binding?.OptC1?.setOnClickListener {
+//            addValues(mycalc, 15,0,0,0,0 )
+//            dta.addValues(15, 0, 0, 0, 0)
+            CALC2.addValues(15, 0, 0, 0, 0)
             navigateToQuestionTwo()
         }
 
         binding?.OptD1?.setOnClickListener {
+//            dta.addValues(0,0,20,0,0 )
+            CALC2.addValues(0, 0, 20, 0, 0)
             navigateToQuestionTwo()
         }
 
@@ -39,4 +53,21 @@ class MainActivity : AppCompatActivity() {
         startActivity(Intent(this, QuestionTwo::class.java))
         finish()
     }
+
+    fun addValues(data: calc, o: Int, c: Int, e: Int, a: Int, n: Int) {
+        data.O += o
+        data.C += c
+        data.E += e
+        data.A += a
+        data.N += n
+    }
+
+    fun subtractValue(data: calc, o: Int, c: Int, e: Int, a: Int, n: Int) {
+        data.O -= o
+        data.C -= c
+        data.E -= e
+        data.A -= a
+        data.N -= n
+    }
+
 }
--- app/PsycheTest/app/src/main/java/com/example/psychetest/QuestionFive.kt
+++ app/PsycheTest/app/src/main/java/com/example/psychetest/QuestionFive.kt
@@ -8,30 +8,51 @@ import com.example.psychetest.databinding.ActivityQuestionThreeBinding
 
 class QuestionFive : AppCompatActivity() {
     private var binding: ActivityQuestionFiveBinding? = null
-
+//    var calc = calc()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuestionFiveBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
         binding?.OptA5?.setOnClickListener {
+            CALC2.addValues(0,30,0,0,0)
             navigateToQuestionTwo()
         }
 
         binding?.OptB5?.setOnClickListener {
+            CALC2.addValues(0,0,0,0,10)
+            CALC2.subtractValue(15,0,0,0,0)
             navigateToQuestionTwo()
         }
 
         binding?.OptC5?.setOnClickListener {
+            CALC2.subtractValue(0,25,0,0,0)
             navigateToQuestionTwo()
         }
 
         binding?.OptD5?.setOnClickListener {
+            CALC2.addValues(20,0,0,0,0)
+            CALC2.subtractValue(0,35,0,0,20)
             navigateToQuestionTwo()
         }
     }
     private fun navigateToQuestionTwo() {
         startActivity(Intent(this, QuestionSix::class.java))
         finish()
+    }
+    fun addValues(data: calc, o: Int, c: Int, e: Int, a: Int, n: Int) {
+        data.O += o
+        data.C += c
+        data.E += e
+        data.A += a
+        data.N += n
+    }
+
+    fun subtractValue(data: calc, o: Int, c: Int, e: Int, a: Int, n: Int) {
+        data.O -= o
+        data.C -= c
+        data.E -= e
+        data.A -= a
+        data.N -= n
     }
 }
